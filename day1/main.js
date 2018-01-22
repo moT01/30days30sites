fc76f84bc50b7841777d6e75e4a85ac9159fa822
Tom <20648924+moT01@users.noreply.github.com>
2018-01-22 13:26:18 -0600

    Update main.js

--- day1/main.js
+++ day1/main.js
@@ -3,13 +3,10 @@ let hamburgerButton = document.getElementById('hamburger');
 let shown = false;
 
 hamburgerButton.addEventListener('click', function() {
-	console.log('click');
    if(shown) {
-     console.log('s');
      navButtons.style.top = "0px";
      shown = false;   
    } else {
-     console.log('us');
      navButtons.style.top = "50px";
      shown = true;
    }
